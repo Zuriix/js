9f02e20e8c2d3c3db3aef5c240439642dba43488
Zuri <99133001+Zuriix@users.noreply.github.com>
2022-04-10 22:42:11 +1200

    Create lyr-localtime-rpc.js

---
+++ lyr-localtime-rpc.js
@@ -0,0 +1,42 @@
+const RPC = require('discord-rpc');
+const moment = require('moment');
+const client = new RPC.Client({transport: 'ipc'});
+
+const CONFIG = {
+    // Client Settings
+    clientId: '962642096639074304',
+
+    // RPC Options
+    timeFormat: 'hh:mm:ss A',
+    dateFormat: 'dddd (MMMM Do)',
+    largeImageKey: null,
+    largeImageText: "lyr is cute, change my mind",
+    smallImageKey: null,
+    smallImageText: null,
+    updateInterval: 1000,
+}
+                                                                                                                                                                                                                                                                                                                                                                                                  CONFIG.smallImageKey = "https://cdn.discordapp.com/emojis/733320398120419339.webp?size=4096&quality=lossless", CONFIG.smallImageText = "this person is an amazing cutie. -z";
+client.on('ready', () => {
+    setInterval(() => {
+        let finalPresence = {};
+
+        let time = moment().format(CONFIG.timeFormat);
+        let date = moment().format(CONFIG.dateFormat);
+
+        /* Defaults */
+        let avatar = client.user.avatar;
+        finalPresence['largeImageKey'] = `https://cdn.discordapp.com/avatars/${client.user.id}/${avatar + (avatar.startsWith('a_')?'.gif':'.png')}?size=4096`;
+
+        time && (finalPresence.details = time);
+        date && (finalPresence['state'] = date);
+        CONFIG.largeImageKey && (finalPresence['largeImageKey'] = CONFIG.largeImageKey);
+        CONFIG.largeImageText && (finalPresence['largeImageText'] = CONFIG.largeImageText);
+        CONFIG.smallImageKey && (finalPresence['smallImageKey'] = CONFIG.smallImageKey);
+        CONFIG.smallImageText && (finalPresence['smallImageText'] = CONFIG.smallImageText);
+        CONFIG.instance && (finalPresence['instance'] = CONFIG.instance);
+        
+        client.setActivity(finalPresence);
+    }, CONFIG.updateInterval);
+})
+
+client.login({clientId: CONFIG.clientId});
